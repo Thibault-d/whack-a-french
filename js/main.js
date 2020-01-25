5e65315013eac361efdfc85c546be411077b2238
Thibault-d <thibaultdelfaud@gmail.com>
2020-01-25 22:00:07 +0100

    HTML & CSS architecture done

---
+++ js/main.js
@@ -0,0 +1,32 @@
+console.log('main.js is connected')
+
+class game {
+    constructor() {
+        this.score = 0;
+        this.timer = 30;
+    }
+
+    start() {
+        document.getElementById('start').onclick = function () {
+            console.log('test')
+        };
+    }
+ 
+
+    updatesScore() {
+
+    }
+
+    timer() {
+
+    }
+
+
+    pause() {
+
+    }
+
+    smash() {
+
+    }
+}  
