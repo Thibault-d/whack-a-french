109c9e03e70793c5dd53dec35a207638b2c7aa26
Thibault-d <thibaultdelfaud@gmail.com>
2020-01-28 20:22:23 +0100

    Create divs from the Game Class

--- js/game.js
+++ js/game.js
@@ -1,16 +1,15 @@
-
 class Game {
     constructor() {
         this.timeLeft = 10;
         this.randomDivs = 9;
-        this.mobs = [1,1,1,1,1,1,1,1,1];
-
+        this.mobs = [1, 1, 1, 1, 1, 1, 1, 1, 1];
     }
 
     start() {
         startButton.onclick = function () {
             console.log('started');
             this.startTimer();
+            this.createDiv();
         }.bind(this)
     }
 
@@ -24,15 +23,16 @@ class Game {
                 document.getElementById('timer').textContent = "TIME OUT";
             }
         }.bind(this), 1000);
+        
     }
 
     createDiv() {
         this.mobs.forEach(element => {
-            
-            
+           let newDiv = document.createElement('div');
+            newDiv.className = 'grid-item';
+            newDiv.textContent = this.mobs[0];
+            document.getElementsByClassName('grid-container')[0].append(newDiv);
+            console.log(newDiv);
         });
-
     }
-
-    generate
 }
