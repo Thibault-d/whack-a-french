a9794e5542c41351c9c8863849035268fce01c76
Thibault-d <thibaultdelfaud@gmail.com>
2020-02-03 23:03:46 +0100

    added more sounds

--- js/mobs.js
+++ js/mobs.js
@@ -1,6 +1,5 @@
 class Mob {
-    constructor() {
-    }
+    constructor() {}
 
     newMob() {
         let mob = mobArray[this.randomIntegerMob()];
@@ -20,57 +19,58 @@ let mobArray = [{
         name: 'sangria',
         scoreValue: -100,
         image: '<img width="150" height="100" src="./img/sangria.png">',
-        frequency: 1,
-        
     },
     {
         name: 'fromage',
         scoreValue: 100,
         image: '<img width="150" height="100" src="./img/fromage.png">',
-        frequency: 1,
     },
     {
         name: 'lepen',
         scoreValue: -200,
         image: '<img width="150" height="100" src="./img/lepen.png">',
-        frequency: 1,
         audio: "./audio/sieg-heil.mp3"
     },
     {
         name: 'tortillas',
         scoreValue: -100,
         image: '<img width="150" height="100" src="./img/tortilla.png">',
-        frequency: 1,
     },
     {
         name: 'seat',
         scoreValue: -100,
         image: '<img width="150" height="100" src="./img/seat.png">',
-        frequency: 1,
     },
     {
         name: '2cv',
         scoreValue: 200,
         image: '<img width="150" height="100" src="./img/2cv.png">',
-        frequency: 1,
+        audio: "./audio/honk.mp3"
     },
     {
         name: 'trump',
         scoreValue: -1000,
         image: '<img width="150" height="100" src="./img/trump.png">',
-        frequency: 1,
+        audio: "./audio/wrong.mp3"
+
     },
     {
         name: 'calimocho',
         scoreValue: -100,
         image: '<img width="120" height="100" src="./img/calimocho.png">',
-        frequency: 1,
+
     },
     {
         name: 'vest',
         scoreValue: 100,
         image: '<img width="120" height="100" src="./img/vest.png">',
-        frequency: 1,
+
+    },
+    {
+        name: 'macdo',
+        scoreValue: -100,
+        image: '<img width="120" height="100" src="./img/macdo.png">',
+
     }
 
 ]
